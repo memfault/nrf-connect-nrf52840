4bfde74f041ac86121ef0901a01a2ba039408964
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-05-17 10:30:44 -0400

    fallback ID when hwinfo is not available

--- memfault_demo_app/src/main.c
+++ memfault_demo_app/src/main.c
@@ -17,24 +17,27 @@ LOG_MODULE_REGISTER(app);
 
 static void set_device_id(void) {
   uint8_t dev_id[16];
-  char dev_str[33];
+  char dev_id_str[33];
+  char *dev_str = "UNKNOWN";
 
   (void)memset(dev_id, 0x0, sizeof(dev_id));
 
   /* Obtain the device id */
   ssize_t length = hwinfo_get_device_id(dev_id, sizeof(dev_id));
 
-  /* If this fails for some reason, use all zeros instead */
+  /* If this fails for some reason, use a fixed ID instead */
   if (length <= 0) {
-    length = sizeof(dev_id);
+    dev_str = CONFIG_SOC_SERIES "-test";
+    length = strlen(dev_str);
+  } else {
+    /* Render the obtained serial number in hexadecimal representation */
+    for (size_t i = 0; i < length; i++) {
+      sprintf(&dev_id_str[i * 2], "%02x", dev_id[i]);
+    }
+    dev_str = dev_id_str;
   }
 
-  /* Render the obtained serial number in hexadecimal representation */
-  for (size_t i = 0; i < length; i++) {
-    sprintf(&dev_str[i * 2], "%02x", dev_id[i]);
-  }
-
-//   printk("Device ID: %s\n", dev_str);
+  printk("Device ID: %s\n", dev_str);
 
   memfault_ncs_device_id_set(dev_str, length * 2);
 }
